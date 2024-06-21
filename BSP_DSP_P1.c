6e9240fe92fb333b2d52492e20db687fb266a9f1
luroel <jl.rodrigueze@hotmail.com>
2024-06-21 13:19:03 -0600

    Diseño y aplicación filtro pasa altas

--- BSP_DSP_P1.c
+++ BSP_DSP_P1.c
@@ -65,3 +65,37 @@ title('Señal Fitrada en tiempo')
 xlabel('seg')
 ylabel('Amplitd')
 end
+if a == 2
+    %---------------------Diseño filtro altas-----------------------------%
+VF1 = vec_frec.*(Fs)/2;
+wc = 60; %Frecuencia de corte
+filter = ((VF1>=-wc)&(VF1<=wc).*(1))+((VF1<-wc)&(VF1>wc).*(0));
+filter2 = 1-filter; %Invertir el filtro
+plot(VF1,filter2,'linewidth',3);
+xlabel('tiempo');
+ylabel('pulse');
+title('filtro pasa altos y Transformada rapida de Fourier Se?al Normalizada');
+hold on 
+grid on
+%---------------------Aplicación del filtro pasa altas--------------------%
+figure
+subplot(2,1,1)
+high_filter = X.*fftshift(filter2); %Porque multiplico por algo que no tiene fftshift
+%high_filter2 = abs(high_filter).^2;
+high_filter2 = fftshift(abs(high_filter).^2);
+%stem(VF,fftshift(high_filter2),'r');
+stem(VF,high_filter2,'r');
+title('Transformada rapida de Fourier Se?al filtrada');
+xlabel('W');
+ylabel('F(w)')
+hold on 
+grid on
+%----------------------Transformada inversa de Fourier--------------------%
+subplot(2,1,2)
+invert_transform = ifft(high_filter);
+plot(t,real(invert_transform));
+grid on
+title('Se?al Fitrada en tiempo')
+xlabel('seg')
+ylabel('Amplitd')
+end
